83e88d9ab1f3ab1763bcfa5c28e78078f4584b3a
Rishav Baral <rishavbaral6@gmail.com>
2020-06-04 02:41:44 +0000

    refactor stdin functions into seperate file input.js

--- play.js
+++ play.js
@@ -1,23 +1,24 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+// const handleUserInput = function(key) {
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+// };
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   stdin.on('data', (key) => {
+//     handleUserInput(key);
+//   });
+//   return stdin;
+// };
 
 connect();
 setupInput();
