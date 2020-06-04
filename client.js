1bfc5b4a9072865de399c2e26dcf367a0b873e17
Rishav Baral <rishavbaral6@gmail.com>
2020-06-04 01:18:24 +0000

    write to my clients console, connection successful, write to the game server my initials and have in show up on the game. also have an .on even handler to console.log any messages from server  onto clients console. this will allow us to see message for when we get kicked out of server for being idle

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: RBB');
+  });
+  conn.on('data', message => {
+    console.log(`${conn.remoteAddress} says ${message}`);
+  });
+  return conn;
+};
+
+module.exports = { connect };
